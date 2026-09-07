85c5bce1968523be9beb29af9c09d637c30d0f4f
HardyF1 <andrewhardy96@gmail.com>
2026-09-07 21:50:59 +0100

    fdsfsfsd

--- hf1_signs/fxmanifest.lua
+++ hf1_signs/fxmanifest.lua
@@ -3,13 +3,18 @@ game 'gta5'
 
 author 'HF1'
 description 'Qbox flat 2D surface signs'
-version '1.2.4'
+version '1.2.5'
 
 lua54 'yes'
 
 shared_scripts {
     '@ox_lib/init.lua',
     'config.lua'
+}
+
+-- Required so the direct-rendered DUI can actually load the local wrapper page.
+files {
+    'html/index.html'
 }
 
 client_scripts {
